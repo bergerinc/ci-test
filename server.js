834d2283e0938c9d6eae5281120c3f2b01ce4738
Bill Berger <bill.berger@csiweb.com>
2019-02-11 01:41:45 -0500

    change to test CI trigger

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 80;
 
 app.get('/', (req,res)=>{
-  res.send('App is running...');
+  res.send('App is running in docker container...');
 });
 
 app.listen(port, () => {
